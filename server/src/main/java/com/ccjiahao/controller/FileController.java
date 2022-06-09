1410b57b2609c1a8fd5d4e2950eff5fd17a2d762
CCjiahao <13462260020@163.com>
2022-06-09 19:00:15 +0800

    添加论文下载功能

--- server/src/main/java/com/ccjiahao/controller/FileController.java
+++ server/src/main/java/com/ccjiahao/controller/FileController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,12 +25,14 @@ import java.util.UUID;
 public class FileController {
 
     private final FileMapper fileMapper;
+
     @Autowired
     public FileController(FileMapper fileMapper) {
         this.fileMapper = fileMapper;
     }
 
     private static final String UPLOADED_FOLDER = new File(System.getProperty("user.dir")).getParent() + File.separator + "uploaded";
+
     @PostMapping("/api/upload")
     public String singleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
         if (file.isEmpty()) {
@@ -46,9 +51,32 @@ public class FileController {
             Dictionary<String, Object> data = new Hashtable<>();
             data.put("uuid", uuid);
             return Feedback.info(data);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             return Feedback.error(e.toString());
         }
+    }
+
+    @GetMapping("/api/download")
+    public String download(String uuid, HttpServletResponse response) {
+        try {
+            FileInputStream inputStream = new FileInputStream(new File(UPLOADED_FOLDER, uuid));
+            com.ccjiahao.entity.File file = fileMapper.selectById(uuid);
+            if (file == null) {
+                return Feedback.error("文件id不正确！");
+            }
+            response.setHeader("content-disposition", "attachment; fileName=" + file.getFilename());
+            ServletOutputStream outputStream = response.getOutputStream();
+            int len = 0;
+            byte[] data = new byte[1024];
+            while ((len = inputStream.read(data)) != -1) {
+                outputStream.write(data, 0, len);
+            }
+            outputStream.close();
+            inputStream.close();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return Feedback.error(e.toString());
+        }
+        return Feedback.info(null);
     }
 }
